120579579489c3a3823b17c06b58cce900a21a40
Vishal <potula.vishal10@gmail.com>
2023-10-07 02:57:39 +0530

    deleted the unwanted comments and displayed the show and actors data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -49,21 +49,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// This code is a React component that represents the home page of a web application.
-
-// The component uses the useState hook from React to manage the state of two variables: apidata and apidataerror.
-
-// The apidata variable is used to store the data retrieved from an API, while the apidataerror variable is used to store any error that occurs during the API request.
-
-// The onsearch function is an asynchronous function that is called when the user performs a search. It takes an object as a parameter containing the search query and the search option (shows or people).
-
-// Inside the onsearch function, it first sets the apidataerror to null to clear any previous error. Then, it makes an API request based on the search option selected. If the search option is "shows", it calls the searchforshows function from the tvmaze API module, otherwise it calls the searchforpeoples function. The result of the API request is stored in the apidata variable.
-
-// If there is an error during the API request, the apidataerror variable is set to the error object.
-
-// The renderapidata function is used to render the API data on the page. If there is an error (apidataerror is not null), it displays an error message. If there is data (apidata is not null), it maps over the data and displays the name of each show or person.
-
-// In the return statement of the component, it renders a Searchform component and the result of the renderapidata function.
-
-// Overall, this code sets up the basic structure for a home page that allows users to search for shows or people and displays the search results
