019a5e9ca251047e32b872527aefa68144a4d752
Vishal <potula.vishal10@gmail.com>
2023-10-10 20:42:23 +0530

    installed react-query and replaced useShowById with usequery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 import Home from "./Pages/Home";
 import Starred from "./Pages/Starred";
 import MainLayout from "./components/MainLayout";
 import Show from "./Pages/Show";
 
+const queryClient = new QueryClient()
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
+
     <BrowserRouter>
     <Routes>
       <Route element={<MainLayout/>}>
@@ -33,6 +37,7 @@ function App() {
       <Route path="contact-us" element={<Contact />} /> */}
     </Routes>
   </BrowserRouter>
+  </QueryClientProvider>
   );
 }
 
--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,28 +1,14 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getShowbyId } from "../API/tvmaze";
-
-const useShowById = (showId) =>{
-  const [showdata , setshowdata] = useState(null);
-  const [showerror, setshowerror] = useState(null);
-
-  useEffect(()=>{
-    async function fetchdata(){
-      try {
-        const data = await getShowbyId(showId);
-        setshowdata(data);
-      } catch (err) {
-        setshowerror(err)
-      }
-    }
-    fetchdata();
-  }, [showId])
-  return {showdata, showerror}
-};
+import {useQuery} from '@tanstack/react-query'
 
 const Show = () => {
     const { showId } = useParams();
-    const {showdata, showerror} = useShowById(showId)
+    // const {showdata, showerror} = useShowById(showId)
+    const {data:showdata, error:showerror} = useQuery({
+      queryKey: ['show',showId],
+      queryFn: () =>getShowbyId(showId)
+    })
 
     if(showerror){
       return <div>We have an Error : {showerror.message} </div>
